594d0936c64d94dadec69e6ab7e606aeb580d8fb
Jose <jose.lopez.s@ucb.edu.bo>
2026-05-19 21:09:15 -0400

    feat: se agrega login

--- Downloads/app3/app/src/main/java/com/example/safewalk/MainActivity.kt
+++ Downloads/app3/app/src/main/java/com/example/safewalk/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat
@@ -11,26 +12,75 @@ import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
+    // Detectar si es premium
+    private var esPremium = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Obtener rol premium
+        esPremium = intent.getBooleanExtra(
+            "premium",
+            false
+        )
+
+        // Permiso micrófono
         verificarPermisoMicrofono()
 
-        val btnNav = findViewById<CardView>(R.id.btnNav)
-        val btnVoice = findViewById<CardView>(R.id.btnSOSManual)
+        // Mostrar tipo usuario
+        if (esPremium) {
 
+            Toast.makeText(
+                this,
+                "⭐ Usuario Premium",
+                Toast.LENGTH_LONG
+            ).show()
+
+        } else {
+
+            Toast.makeText(
+                this,
+                "👤 Modo invitado",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        // Botones
+        val btnNav =
+            findViewById<CardView>(R.id.btnNav)
+
+        val btnVoice =
+            findViewById<CardView>(R.id.btnSOSManual)
+
+        // Ruta segura
         btnNav.setOnClickListener {
 
-            val intent = Intent(this, RutaActivity::class.java)
+            val intent =
+                Intent(this, RutaActivity::class.java)
+
             startActivity(intent)
         }
 
+        // Emergencia por voz
         btnVoice.setOnClickListener {
 
-            val intent = Intent(this, VoiceEmergencyActivity::class.java)
+            val intent =
+                Intent(
+                    this,
+                    VoiceEmergencyActivity::class.java
+                )
+
             startActivity(intent)
+        }
+
+        // EJEMPLO PREMIUM
+        // Aquí luego puedes desbloquear funciones
+        if (!esPremium) {
+
+            // Ejemplo:
+            // btnPremium.isEnabled = false
         }
     }
 
@@ -45,7 +95,9 @@ class MainActivity : AppCompatActivity() {
 
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf(Manifest.permission.RECORD_AUDIO),
+                arrayOf(
+                    Manifest.permission.RECORD_AUDIO
+                ),
                 100
             )
         }
